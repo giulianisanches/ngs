3a17b57f7794fb6e51d20520f41ae2a8e230f7b8
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-07-07 17:07:52 -0300

    feat: resolve 8-challenge2
    Needed to do a google search as i was not aware
    of the possibility to call the clearInterval
    withing the setInterval callback

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -1,5 +1,19 @@
 // Print "Hello World"
 // Every second
 // And stop after 5 times
+let counter = 0;
+
+const hello = setInterval(msg => {
+    console.log(msg);
+
+    counter++;
+
+    if (counter === 5) {
+        console.log("Done")
+        clearInterval(hello)
+    }
+}, 1000, "Hello World");
+
+//clearInterval(hello)
 
 // After 5 times. Print "Done" and let Node exit.
